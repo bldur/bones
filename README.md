c0464bde1b4063448c71d28d55d91a7c7a30c1c7
Your Name <you@example.com>
2022-09-20 06:21:22 +0200

    depends on grepcidr and prips now, but works

--- README.md
+++ README.md
@@ -1,53 +1,18 @@
 # bones
 # scripts and no fresh meat
 
-Awk needs to be shafted unless someone that knows awk well write up.
 
-These two programs:
+Works for openwrt 22.03 with these two programs:
 https://github.com/jrlevine/grepcidr3
 https://gitlab.com/prips/prips
 
-Which together with generic grep, sort and diff can do what i want.
+netblocks smaller than 24 get converted to single ip.
+several rounds of regex to process.
+checks for overlapping netblocks and unique addresses to not get nft ipset crashes.
 
-# broken without above.
+never flushes, adds and removes addresses.
+keeps last aggregated raw blocklist in /tmp/badhost
 
-Told runtime for VoIPBL voipbl.org/ on i5 is 7 seconds on openwrt 22.03 using nft.
+has whitelist and support for logging addresses one want to be alerted about found in blocklists.
 
-Works such that giving it a blank blocklist will remove all ip's from the ipset.
-
-This is just a mess of with reasonable regex, two awk programs mangled to work.
-AWK programs taken from UNIX.com and regex from pf-badhost and another from oreilly.com.
-
-Function to batch process from stackexchange, etc.
-So mostly combining and finding the simple, dumb, old and reasonably fast.
-
-Easy to adapt and change, as there is no "meat", just basic functionality bones.
-Where perhaps only IPv4 is useful for public blocklists.
-A missing feature is whitelisting, which grep -vf file1 downloaded_lists_parsed could do.
-
-Focus is on greedily coaxing and forcing individual ip's in the blocklist.
-If it can't convert a CIDR /32 all /xx are stripped.
-
-
-On ARM64 Cortex 72A clocked to 1Ghz.
-
-# time sh wrt-badhost.sh
-
-real	0m35.103s
-user	0m27.306s
-sys	0m5.850s
-
-Here it downloaded 644 KB blocklist text involving 37119 IPv4 addresses.
-Created a diff of NFT ipset and compared.
-
-Adding 50 IP's to the set, and removing 0, as none had been removed since last download.
-
-And empty blocklist file is the same as removing or flushing the ipset.
-
-# puzzling intermittently
-
-Haven't myself tested just giving it various /24 to properly test everything involved
-and what it does with /8 apart from noticing that it is not quite right and for ipv4 ipset
-with interval flag turned on and not stripping this.
-
-It moves away from greedily catch-all where perhaps various service specific blocklists for log spam.
+failure to download will clear nft ipset.
--- wrt-badhost.sh
+++ wrt-badhost.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-#LC_ALL=C
-
+#turn off to not see what script does
 #set -x
 
 # IP blacklisting script for openwrt nftables
@@ -11,209 +10,35 @@
 # https://www.geoghegan.ca/pfbadhost.html
 # copyleft, restrictions from above sources may apply.
 
-# scroll past useful functions and awk programs merely mangled and not written.
+# only whitelisting ipv4 currently, don't have to overlap with alarm list.
+# shoot yourself, failure to download is the same as flushing ipset.
+WHITELIST_FILE=""
+# does logger and ip found in raw blocklist
+# as logger -s "ALERT: $i found in blocklists"
+# relies on grepcidr and takes raw blocklist and takes also ipv6
+BLOCKLIST_ALARM=""
 
-AWK_CIDR32() {
-awk -e '
-function range2cidr(ipStart, ipEnd,  bits, mask, newip) {
-    bits = 1
-    mask = 1
-    result = ""
-    while (bits < 32) {
-        newip = or(ipStart, mask)
-        if ((newip>ipEnd) || ((lshift(rshift(ipStart,bits),bits)) != ipStart)) {
-           bits--
-           mask = rshift(mask,1)
-           break
-        }
-        bits++
-        mask = lshift(mask,1)+1
-    }
-    newip = or(ipStart, mask)
-    bits = 32 - bits
-    result = result dec2ip(ipStart) "/" bits
-    if (newip < ipEnd) result = result "\n" range2cidr(newip + 1, ipEnd)
-    return result
-}
+URLS="
+http://www.voipbl.org/update/
+https://lists.blocklist.de/lists/all.txt
+https://www.team-cymru.org/Services/Bogons/bogon-bn-nonagg.txt
+https://rules.emergingthreats.net/fwrules/emerging-Block-IPs.txt
+https://rules.emergingthreats.net/blockrules/compromised-ips.txt
+https://www.binarydefense.com/banlist.txt
+https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/firehol_level1.netset
+https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/firehol_level2.netset
+https://isc.sans.edu/api/threatlist/shodan/?text
+https://feodotracker.abuse.ch/downloads/ipblocklist.txt
+https://sslbl.abuse.ch/blacklist/sslipblacklist.txt
+https://www.spamhaus.org/drop/dropv6.txt
+https://www.spamhaus.org/drop/drop.txt
+https://www.spamhaus.org/drop/edrop.txt
+https://www.stopforumspam.com/downloads/toxic_ip_cidr.txt
+https://www.stopforumspam.com/downloads/listed_ip_1_ipv46.gz
+https://ozgur.kazancci.com/ban-me.txt
+https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/firehol_level3.netset
+"
 
-# convert dotted quads to long decimal ip
-#       int ip2dec("192.168.0.15")
-#
-function ip2dec(ip,   slice) {
-        split(ip, slice, ".")
-        return (slice[1] * 2^24) + (slice[2] * 2^16) + (slice[3] * 2^8) + slice[4]
-}
-
-# convert decimal long ip to dotted quads
-#       str dec2ip(1171259392)
-#
-function dec2ip(dec,    ip, quad) {
-        for (i=3; i>=1; i--) {
-                quad = 256^i
-                ip = ip int(dec/quad) "."
-                dec = dec%quad
-        }
-        return ip dec
-}
-
-
-# convert decimal ip to binary
-#       str dec2binary(1171259392)
-#
-function dec2binary(dec,    bin) {
-        while (dec>0) {
-                bin = dec%2 bin
-                dec = int(dec/2)
-        }
-        return bin
-}
-
-# Convert binary ip to decimal
-#       int binary2dec("1000101110100000000010011001000")
-#
-function binary2dec(bin,   slice, l, dec) {
-        split(bin, slice, "")
-        l = length(bin)
-        for (i=l; i>0; i--) {
-                dec += slice[i] * 2^(l-i)
-        }
-        return dec
-}
-
-# convert dotted quad ip to binary
-#       str ip2binary("192.168.0.15")
-#
-function ip2binary(ip) {
-        return dec2binary(ip2dec(ip))
-}
-
-
-# count the number of ips in a dotted quad ip range
-#       int countIp ("192.168.0.0" ,"192.168.1.255") + 1
-#
-function countQuadIp(ipStart, ipEnd) {
-        return (ip2dec(ipEnd) - ip2dec(ipStart))
-}
-
-
-# count the number of ips in a CIDR block
-#       int countCidrIp ("192.168.0.0/12")
-#
-function countCidrIp (cidr) {
-        sub(/.+\//, "", cidr)
-        return 2^(32-cidr)
-}
-
-function sanitize(ip) {
-        split(ip, slice, ".")
-        return slice[1]/1 "." slice[2]/1 "." slice[3]/1 "." slice[4]/1
-}
-
-BEGIN{
-       FS=" , | - "
-       print "-N cidr nethash --maxelem 260000\n-N single iphash --maxelem 60000\n"
-}
- 
-# sanitize ips
-{$1 = sanitize($1); $2 = sanitize($2)}
-
-# range with a single IP
-#$1==$2 {printf "-A single %s\n", $1} 
-
-# ranges with multiple IPs
-$1!=$2{print range2cidr(ip2dec($1), ip2dec($2))}
-
-
-# footer
-#END {print "COMMIT\n"}
-'
-}
-
-AWK_IPRANGE() {
-awk -e '
-# Convert IP ranges to CIDR notation
-# awk, gawk, mawk compatible
-
-function range2cidr(ipStart, ipEnd, result, bits, mask, newip) {
-    bits = 1
-    mask = 1
-    while (bits < 32) {
-        newip = bit_or(ipStart, mask)
-        if ((newip > ipEnd) || ((bit_lshift(bit_rshift(ipStart,bits),bits)) != ipStart)) {
-            bits--
-            mask = bit_rshift(mask,1)
-            break
-        }
-        bits++
-        mask = bit_lshift(mask,1)+1
-    }
-    newip = bit_or(ipStart, mask)
-    bits = 32 - bits
-    result = (result)?result ORS dec2ip(ipStart) "/" bits : dec2ip(ipStart) "/" bits
-    if (newip < ipEnd) result = range2cidr(newip + 1, ipEnd,result)
-    return result
-}
-
-# convert dotted quads to long decimal ip
-# int ip2dec("192.168.0.15")
-#
-function ip2dec(ip, slice) {
-    split(ip, slice, /[.]/)
-    return (slice[1] * 2^24) + (slice[2] * 2^16) + (slice[3] * 2^8) + slice[4]
-}
-
-# convert decimal long ip to dotted quads
-# str dec2ip(1171259392)
-#
-function dec2ip(dec, ip, quad) {
-    for (i=3; i>=1; i--) {
-        quad = 256^i
-        ip = ip int(dec/quad) "."
-        dec = dec%quad
-    }
-    return ip dec
-}
-
-# Bitwise OR of var1 and var2
-function bit_or(a, b, r, i, c) {
-    for (r=i=0;i<32;i++) {
-        c = 2 ^ i
-        if ((int(a/c) % 2) || (int(b/c) % 2)) r += c
-    }
-    return r
-}
-
-# Rotate bytevalue left x times
-function bit_lshift(var, x) {
-    while(x--) var*=2;
-    return var;
-}
-
-# Rotate bytevalue right x times
-function bit_rshift(var, x) {
-    while(x--) var=int(var/2);
-    return var;
-}
-
-function sanitize(ip) {
-    split(ip, slice, /[.]/)
-    return slice[1]/1 "." slice[2]/1 "." slice[3]/1 "." slice[4]/1
-}
-
-BEGIN{
-    FS=" - |-|:"
-}
-
-# sanitize ips
-!/^#/ && NF {
-    f1 = sanitize($(NF-1))
-    f2 = sanitize($NF)
-    print range2cidr(ip2dec(f1), ip2dec(f2))
-}
-
-END {print ""}
-'
-}
 
 GREP_IPV4() {
 	grep -E -o -- '((25[0-5]|(2[0-4]|1{0,1}[[:digit:]]){0,1}[[:digit:]])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[[:digit:]]){0,1}[[:digit:]])(/(3[0-2]|[1-2][[:digit:]]|[1-9]))?'
@@ -227,8 +52,35 @@ GREP_IPV6() {
 	grep -E -x -- '(([[:xdigit:]]{1,4}:){7,7}[[:xdigit:]]{1,4}|([[:xdigit:]]{1,4}:){1,7}:|([[:xdigit:]]{1,4}:){1,6}:[[:xdigit:]]{1,4}|([[:xdigit:]]{1,4}:){1,5}(:[[:xdigit:]]{1,4}){1,2}|([[:xdigit:]]{1,4}:){1,4}(:[[:xdigit:]]{1,4}){1,3}|([[:xdigit:]]{1,4}:){1,3}(:[[:xdigit:]]{1,4}){1,4}|([[:xdigit:]]{1,4}:){1,2}(:[[:xdigit:]]{1,4}){1,5}|[[:xdigit:]]{1,4}:((:[[:xdigit:]]{1,4}){1,6})|:((:[[:xdigit:]]{1,4}){1,7}|:)|[fF][eE]80:(:[[:xdigit:]]{0,4}){0,4}%[[:alnum:]]{1,}|::([fF]{4}(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[[:digit:]]){0,1}[[:digit:]])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[[:digit:]]){0,1}[[:digit:]])|([[:xdigit:]]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[[:digit:]]){0,1}[[:digit:]])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[[:digit:]]){0,1}[[:digit:]]))(/(12[0-8]|1[0-1][[:digit:]]|[1-9][[:digit:]]{0,1}))?'
 }
 
+# not perfect, takes 1.2.3.4.5 as 2.3.4.5, spaces, new lines from back
+GREP_IPV4_NO_CIDR() {
+	 grep -E -o -- "(\b25[0-5]|\b2[0-4][0-9]|\b[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}($|\s)"
+}
+
+# matches 0.0.0.0-0.0.0.0 and 0.0.0.0 - 0.0.0.0
+# not 0.0.0.0-
+#0.0.0.0
+# or anything not space and dash, and doesn't understand / or --
+# warning, accepts up to 20 spaces or tabs on either side of -
+# as for comlete documentation of these:
+# https://www.oreilly.com/library/view/regular-expressions-cookbook/9780596802837/ch07s16.html
+GREP_IPV4_RANGE_NO_CIDR() {
+	grep -E -o -- "(\b25[0-5]|\b2[0-4][0-9]|\b[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}(\b\s{0,20}-\s{0,20})(\b25[0-5]|\b2[0-4][0-9]|\b[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}"
+}
+
+# regex to get all ipv4 /24 or bigger, or /25 until /32
+GREP_CIDR_0_24() {
+	grep -E -o -- "(\b25[0-5]|\b2[0-4][0-9]|\b[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}\b/([0-9]\>|[1][0-9]\>|[2][0-4]\>)"
+}
+
+GREP_CIDR_25_32() {
+	grep -E -o -- "(\b25[0-5]|\b2[0-4][0-9]|\b[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}([/][2][5-9]|[/][3][0-2])\>"
+}
+
+
+
 DIFFER() {
-	 diff -N --old-line-format=$'delete %l\n' --new-line-format=$'add %l\n'  --unchanged-line-format='' "$@"
+	 diff -N --old-line-format=$'del %l\n' --new-line-format=$'add %l\n'  --unchanged-line-format='' "$@"
 }
 
 # a function to use in while loops to batch process.
@@ -257,48 +109,18 @@ function readlines () {
     return $rc
 }
 
-GREP_IPV4_NO_CIDR() {
-	grep -E -o "(\b25[0-5]|\b2[0-4][0-9]|\b[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}"
-}
-
-GREP_IPV4_RANGE_NO_CIDR() {
-	grep -E -o "(\b25[0-5]|\b2[0-4][0-9]|\b[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}?.-?.(\b25[0-5]|\b2[0-4][0-9]|\b[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}"
-}
-
 	
-
-# some blocklists 
-#https://www.team-cymru.org/Services/Bogons/bogon-bn-nonagg.txt
-#https://www.blocklist.de/downloads/export-ips_all.txt
-
-URLS="
-https://rules.emergingthreats.net/fwrules/emerging-Block-IPs.txt
-https://rules.emergingthreats.net/blockrules/compromised-ips.txt
-https://www.binarydefense.com/banlist.txt
-https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/firehol_level1.netset
-https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/firehol_level2.netset
-https://isc.sans.edu/api/threatlist/shodan/?text
-https://feodotracker.abuse.ch/downloads/ipblocklist.txt
-https://sslbl.abuse.ch/blacklist/sslipblacklist.txt
-https://www.spamhaus.org/drop/dropv6.txt
-https://www.spamhaus.org/drop/drop.txt
-https://www.spamhaus.org/drop/edrop.txt
-https://www.stopforumspam.com/downloads/toxic_ip_cidr.txt
-https://www.stopforumspam.com/downloads/listed_ip_1_ipv46.gz
-https://ozgur.kazancci.com/ban-me.txt
-https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/firehol_level3.netset
-"
 
 # create set to contain ip addresses
 if ! nft list set inet fw4 blackhole6 > /dev/null 2> /dev/null; then
-  nft add set inet fw4 blackhole { type ipv4_addr\; }
+  nft add set inet fw4 blackhole { type ipv4_addr\; flags interval\; }
   nft add set inet fw4 blackhole6 { type ipv6_addr\; flags interval\; }
 fi
 
 # insert chain to drop where source address in blackhole set
 if ! nft list chain inet fw4 input_wan | grep -q blackhole; then
-  nft insert rule inet fw4 input_wan ip saddr @blackhole drop
-  nft insert rule inet fw4 input_wan ip6 saddr @blackhole6 drop
+  nft insert rule inet fw4 input_wan ip saddr @blackhole reject
+  nft insert rule inet fw4 input_wan ip6 saddr @blackhole6 reject
 fi
 
 # temp folder and keep a few days
@@ -318,6 +140,15 @@ nft_ipv4=$(mktemp -t nft_ipv4.XXXXXX)
 nft_ipv4_list=$(mktemp -t nft_ipv4_list.XXXXXX)
 nft_ipv6=$(mktemp -t nft_ipv6.XXXXXX)
 nft_ipv6_list=$(mktemp -t nft_ipv6_list.XXXXXX)
+cidr_ipv4=$(mktemp -t cidr_ipv4.XXXXXX)
+cidr_ipv4_unmerge=$(mktemp -t cidr_ipv4_unmerge.XXXXXX)
+single_ipv4=$(mktemp -t single_ipv4.XXXXXX)
+single_nft_ipv4=$(mktemp -t single_nft_ipv4.XXXXXX)
+cidr_nft_ipv4=$(mktemp -t cidr_nft_ipv4.XXXXXX)
+diff_single_ipv4=$(mktemp -t diff_single_ipv4.XXXXXX)
+diff_cidr_ipv4=$(mktemp -t diff_cidr_ipv4.XXXXXX)
+diff_input_nft_ipv4=$(mktemp -t diff_input_nft_ipv4.XXXXXX)
+diff_input_blocklist_ipv4=$(mktemp -t diff_input_blocklist_ipv4.XXXXXX)
 
 #blocklist_ipv6=$(mktemp -p $TMPDIR)
 #diff_add_del_ipv6=$(mktemp -p $TMPDIR)
@@ -329,54 +160,99 @@ for url in $URLS; do
     echo
 done  > "${blocklist}"
 
+echo downloaded blocklists $(date)
+
 # get an ip list from the nft set
 #nft list set inet fw4 blackhole  > "${nft_ipv4}"
 #cat "${nft_ipv4}" | GREP_IPV4 | AWK_CIDR32 | GREP_IPV4_NO_CIDR | sort -u > "${nft_ipv4_list}"
-nft list set inet fw4 blackhole | GREP_IPV4 | sort -u > "${nft_ipv4_list}"
+nft list set inet fw4 blackhole | GREP_IPV4 > "${nft_ipv4}"
 nft list set inet fw4 blackhole6 | GREP_V_COM | awk '$1=$1' RS="," OFS="\n" | GREP_IPV6 | sort -u > "${nft_ipv6_list}"
 
+# hmm
+cat "${nft_ipv4}" | GREP_IPV4 > "${nft_ipv4_list}"
+cat "${nft_ipv4_list}" | GREP_CIDR_0_24  > "${cidr_nft_ipv4}"
+cat "${nft_ipv4_list}" | GREP_CIDR_25_32 | while read line ; do
+	prips $line
+done > "${single_nft_ipv4}"
+cat "${nft_ipv4_list}" | GREP_IPV4_NO_CIDR >> "${single_nft_ipv4}"
+# just in case of auto-merge, hmm, two ipsets
+
+# check
+if test -s "${BLOCKLIST_ALARM}"; then
+        cat "${BLOCKLIST_ALARM}" | while read line ; do
+                grepcidr -i $line "${blocklist}" && logger "ALERT: $line found in blocklists"
+        done
+fi
 # same from downloaded raw file
-cat "${blocklist}" | GREP_IPV4 | AWK_CIDR32 | GREP_IPV4_NO_CIDR | sort -u > "${blocklist_ipv4}"
 cat "${blocklist}" | GREP_V_COM | awk -- '{print $1}' | GREP_IPV6 | sort -u > "${blocklist_ipv6}"
+#ipv4
+if test -s "${WHITELIST_FILE}" ;then
+	cat "${blocklist}" | GREP_IPV4 | grepcidr -vf ${WHITELIST_FILE} | sort -u > "${blocklist_ipv4}"
+	else
+	cat "${blocklist}" | GREP_IPV4 | sort -u > "${blocklist_ipv4}"
+fi
+cat "${blocklist_ipv4}" | GREP_CIDR_0_24 > "${cidr_ipv4_unmerge}"
+cat "${blocklist_ipv4}" | GREP_CIDR_25_32 | while read line ; do
+	prips $line
+done | grepcidr -vf "${cidr_ipv4_unmerge}" > "${single_ipv4}"
+cat "${cidr_ipv4_unmerge}" | while read line ;do
+	grepcidr $line "${cidr_ipv4_unmerge}"
+done | uniq -u | grepcidr -v  "${cidr_ipv4_unmerge}" > "${cidr_ipv4}"
+cat "${blocklist_ipv4}" | GREP_IPV4_NO_CIDR  >> "${single_ipv4}"
 
 # make a new file with diff, "add ip" and "delete ip", one line per ip with diff
-DIFFER "${nft_ipv4_list}" "${blocklist_ipv4}" | sort -u > "${diff_add_del_ipv4}";
-DIFFER "${nft_ipv6_list}" "${blocklist_ipv6}" | sort -u > "${diff_add_del_ipv6}";
+#DIFFER "${single_nft_ipv4}" "${single_ipv4}"  > "${diff_single_ipv4}"
+#DIFFER "${cidr_nft_ipv4}" "${cidr_ipv4}"  > "${diff_cidr_ipv4}"
+cat "${single_nft_ipv4}" "${cidr_nft_ipv4}" | sort -u > "${diff_input_nft_ipv4}"
+cat "${single_ipv4}" "${cidr_ipv4}" | sort -u > "${diff_input_blocklist_ipv4}"
+DIFFER "${diff_input_nft_ipv4}" "${diff_input_blocklist_ipv4}"  > "${diff_add_del_ipv4}"
+DIFFER "${nft_ipv6_list}" "${blocklist_ipv6}" > "${diff_add_del_ipv6}"
 
 # and we are fairly certain we only send valid IP's to nft
 # slowest part outside of download
 
 # add and delete ipv4 for ipset
+echo Start of nft ipset operations $(date)
 grep add "${diff_add_del_ipv4}" | cut -d ' ' -f 2 | while batch=$(readlines 10000); do
 	echo $batch | awk '$1=$1' RS= OFS=", " | while read line ;do
 	nft add element inet fw4 blackhole { $line }
+	echo iteration of nft ipv4 add
 	done
 done
-grep delete "${diff_add_del_ipv4}" | cut -d ' ' -f 2 | while batch=$(readlines 10000); do
+grep del "${diff_add_del_ipv4}" | cut -d ' ' -f 2 | while batch=$(readlines 10000); do
         echo $batch | awk '$1=$1' RS= OFS=", " | while read line ;do
         nft delete element inet fw4 blackhole { $line }
+	echo iteration of nft ipv4 delete
         done
 done
 
-# add and delete ipv6 from ipset
+# add and del ipv6 from ipset
 grep add "${diff_add_del_ipv6}" | cut -d ' ' -f 2 | while batch=$(readlines 10000); do
         echo $batch | awk '$1=$1' RS= OFS=", " | while read line ;do
         nft add element inet fw4 blackhole6 { $line }
+	echo iteration of nft ipv6 add
         done
 done
-grep delete "${diff_add_del_ipv6}" | cut -d ' ' -f 2 | while batch=$(readlines 10000); do
+grep del "${diff_add_del_ipv6}" | cut -d ' ' -f 2 | while batch=$(readlines 10000); do
         echo $batch | awk '$1=$1' RS= OFS=", " | while read line ;do
         nft delete element inet fw4 blackhole6 { $line }
+	echo iteration of nft ipv6 delete
         done
 done
 
 # keep the last raw blocklist
 gzip -9n "${blocklist}" -c > /tmp//badhost/last_raw_blocklist.gz
 
+echo "wrt-badhost.sh completed $(date)"
+echo "total ipv4 elements in list: $(wc -l "${blocklist_ipv4}")"
+echo "total ipv6 elements in list: $(wc -l "${blocklist_ipv6}")"
+echo "Amount of ipv4 entries before changes: $(wc -l "${nft_ipv4_list}")"
+echo "ipv4 added: $(grep add "${diff_add_del_ipv4}" | wc -l)"
+echo "ipv4 removed: $(grep del "${diff_add_del_ipv4}" | wc -l)"
+echo "ipv6 added: $(grep add "${diff_add_del_ipv6}" | wc -l)"
+echo "ipv6 removed: $(grep del "${diff_add_del_ipv6}" | wc -l)"
+
 if [ "$TMPDIR" = "/tmp/badhost/wrk" ]; then
 	rm -rf $TMPDIR
 fi
-#rm -rf /tmp/badhost
-#mv "${blocklist_ipv4}" "${persistent_blocklist}"
-
 # done!
